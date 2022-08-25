927ef55b6fa43ae0e2e7193e4ef9553a64bd121f
xjomino <jammwa@gmail.com>
2022-08-25 13:29:27 +0300

    trying variables

---
+++ variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	printf("My name is james.\n" );
+	printf("I am 33 years old.\n");
+	printf("I love coding and developing solutions.\n");
+	printf("I have 2 cars and a dog. \n");
+	printf("I have a passion for business.\n");
+}
